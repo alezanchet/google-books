974cf1603dcdb84d7af865ba9ecf89838e9f78de
alezanchet <alexandrezanchet@gmail.com>
2020-12-08 10:45:13 -0300

    Eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,9 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    jest: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -16,11 +12,21 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'react-hooks',
-    'prettier'
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
   },
 };
--- App.js
+++ App.js
@@ -1,49 +1,49 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
-import firebase from 'firebase'
-
+import firebase from 'firebase';
 
 export default function App() {
-  const [loggedIn, setLoggedIn] = useState(null)
+  const [loggedIn, setLoggedIn] = useState(null);
 
   useEffect(() => {
     firebase.initializeApp({
-      apiKey: "AIzaSyC-UPFTWT4ocq79PrIBKVwyC_GY3yIa7Gw",
-      authDomain: "books-44061.firebaseapp.com",
-      projectId: "books-44061",
-      storageBucket: "books-44061.appspot.com",
-      messagingSenderId: "6714482192",
-      appId: "1:6714482192:web:f551d1aa6d1fcade093495",
-      measurementId: "G-NGT80ZRQJY"
-    })
+      apiKey: 'AIzaSyC-UPFTWT4ocq79PrIBKVwyC_GY3yIa7Gw',
+      authDomain: 'books-44061.firebaseapp.com',
+      projectId: 'books-44061',
+      storageBucket: 'books-44061.appspot.com',
+      messagingSenderId: '6714482192',
+      appId: '1:6714482192:web:f551d1aa6d1fcade093495',
+      measurementId: 'G-NGT80ZRQJY',
+    });
 
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        setLoggedIn(true)
+        setLoggedIn(true);
       } else {
-        setLoggedIn(false)
+        setLoggedIn(false);
       }
-    })
-  },[])
+    });
+  }, []);
 
   const renderContent = () => {
     switch (loggedIn) {
       case true:
         return (
-          <Button
-            onPress={() => firebase.auth().signOut()}
-          >Log Out</Button>
-        )
+          <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+        );
       case false:
-        return <LoginForm />
+        return <LoginForm />;
       default:
-        return <Spinner size="large" />
+        return <Spinner size="large" />;
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
       {renderContent()}
+      <View>
+        <Text>oi</Text>
+      </View>
     </View>
   );
 }
